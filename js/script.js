1fb3e91097f07317c291dd1e5c12ca4fb1a70ad5
pzh2000 <kunmichael.han@gmail.com>
2019-02-17 14:46:40 -0700

    Get the storyboard ready to use.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@ function Init()
 {
 	var counter = 0;	//Counter for click on story board
 	var story_board = getElementById("storyboard");
+	var current_message = getElementById("storyboard__message--title");
+	var next_message;
 	story_board.addEventListener("click", function(){
-		
+		counter++;
+		current_message.toggle("storyboard__message--active");
+		next_message = getElementById("storyboard__message--pt" + counter);
+		next_message.toggle("storyboard__message--active");
+		current_message = next_message;
 	}); 
 }
