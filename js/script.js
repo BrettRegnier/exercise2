c2347e11b46fd03c8c7a23ce71c87d52fd3371e9
pzh2000 <kunmichael.han@gmail.com>
2019-02-17 14:48:31 -0700

    Fixed the missing document object

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ window.onload = Init;
 function Init()
 {
 	var counter = 0;	//Counter for click on story board
-	var story_board = getElementById("storyboard");
-	var current_message = getElementById("storyboard__message--title");
+	var story_board = document.getElementById("storyboard");
+	var current_message = document.getElementById("storyboard__message--title");
 	var next_message;
 	story_board.addEventListener("click", function(){
 		counter++;
 		current_message.toggle("storyboard__message--active");
-		next_message = getElementById("storyboard__message--pt" + counter);
+		next_message = document.getElementById("storyboard__message--pt" + counter);
 		next_message.toggle("storyboard__message--active");
 		current_message = next_message;
 	}); 
