89d037f6f4062df819e0783b8f6a1e0813be3ded
pzh2000 <kunmichael.han@gmail.com>
2019-02-17 14:29:34 -0700

    Added id to storyboard divs, and worked on js.

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ window.onload = Init;
 
 function Init()
 {
-	
+	var counter = 0;	//Counter for click on story board
+	var story_board = getElementById("storyboard");
+	story_board.addEventListener("click", function(){
+		
+	}); 
 }
